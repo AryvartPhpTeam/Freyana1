df0839a97bea36790ce315ff666c991d5b46f53c
nivethaRa <nivetha@aryvart.com>
2016-01-27 13:56:22 +0530

    updatecommit

---
+++ wp-content/themes/freyana/index.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>Freyana - Jewellery Collections</title>
+<link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.css" rel="stylesheet">
+<link href="<?php echo get_template_directory_uri(); ?>/css/font-awesome.css" rel="stylesheet">
+<link href="<?php echo get_template_directory_uri(); ?>/css/owl.carousel.css" rel="stylesheet">
+<link href="<?php echo get_template_directory_uri(); ?>/css/owl.theme.css" rel="stylesheet">
+<link href="<?php echo get_template_directory_uri(); ?>/css/style.css" rel="stylesheet">
+<link href="<?php echo get_template_directory_uri(); ?>/css/modal.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="modal-1" class="modal_box">
+  <div class="content-popup">
+    <div class="model-details">
+      <h2>JOIN OUR FAN CLUB</h2>
+      <p>Join our Fan Club to enjoy 15% off on your first purchase and other exclusive deals on the hottest trends.</p>
+      <input type="text" value="" placeholder="Name" required />
+      </br>
+      <input type="text" value="" placeholder="Email" required />
+      </br>
+      <input type="button" value="Sign Up" class="submit_btn" />
+    </div>
+  </div>
+</div>
+<section class="header-block">
+  <div class="container">
+    <div class="row">
+      <div class="header-top">
+        <div class="col-lg-8 col-md-8 col-sm-12">
+          <div class="menu">
+            <nav class="navbar navbar-default">
+              <div class=""> 
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+                  <a class="navbar-brand visible-xs" href="#">Menu</a> </div>
+                
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav">
+                    <li><a href="#">About Freyana <span class="sr-only">(current)</span></a></li>
+                    <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Our Collections <span class="caret"></span></a>
+                      <ul class="dropdown-menu">
+                        <li><a href="collections.html">Queen of Hearts</a></li>
+                        <li><a href="#">Beloved</a></li>
+                        <li><a href="#">Adored</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="#">Lookbook </a></li>
+                    <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">The World of Freyana <span class="caret"></span></a>
+                      <ul class="dropdown-menu">
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Press</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="contact.html">Contact Us</a></li>
+                  </ul>
+                </div>
+                <!-- /.navbar-collapse --> 
+              </div>
+              <!-- /.container-fluid --> 
+            </nav>
+          </div>
+          <!-- menu --> 
+          <!-- <div class="logo">
+                <a href="index.html"><img src="images/logo.png" class="img-responsive"></a>
+              </div> --> 
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+          <div class="cart-header">
+            <div class="cart">
+              <ul class="list-inline f-left">
+                <li><span><i class="fa fa-cart-arrow-down"></i> Shopping Bag(0)</span></li>
+                <li>
+                  <input type="text" class="form-control" placeholder="Search" >
+                  <i class="fa fa-search"></i></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- header-block -->
+<section class="banner-block">
+  <div class="slider_container">
+    <div class="flexslider">
+      <ul class="slides">
+        <li> <img src="<?php echo get_template_directory_uri(); ?>/images/banner01.jpg" class="img-responsive"/>
+          <div class="flex-caption">
+            <div class="caption_title_line">
+              <div class="logo"><a href="index.html"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" class="img-responsive"></a></div>
+              <p>Where every piece makes a statement</p>
+              <a href="#" class="btn btn-default"><i class="fa fa-caret-left"></i> SHOP NOW <i class="fa fa-caret-right"></i></a> </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
+<!-- banner-block -->
+<section class="new-collection">
+  <div class="container">
+    <div class="row">
+      <h2>OUR COLLECTIONS </h2>
+      <div class="clearfix featured-collection">
+        <div class="col-md-4 col-sm-4 ">
+          <div class="featured-details">
+            <div class="queenofhearts">
+              <div class="collectionimg"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/queen_hearts.png" class="img-responsive" /></a></div>
+              <a href="#"><h1>Queen of Hearts</h1></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-4">
+          <div class="featured-details">
+            <div class="adored">
+              <div class="collectionimg"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/adored_col.png" class="img-responsive" /></a></div>
+              <a href="#"><h1>Adored</h1></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-4">
+          <div class="featured-details">
+            <div class="beloved">
+              <div class="collectionimg"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/beloved_col.png" class="img-responsive" /></a></div>
+              	<a href="#"><h1>Beloved</h1></a>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- new-collection -->
+<section class="jewellery-collection clearfix">
+  <div class="container">
+    <div class="row"> 
+      <!-- <div class="head-line hidden-xs hidden-sm hidden-md">Freyana</div> -->
+      <div class="j-collection">
+        <h2>Featured In </h2>
+        <div class="featured clearfix">
+          <div class="col-md-4 col-sm-4">
+            <div class="fea-details"> <span><a href="http://instoremag.com/products/opening-lines-november-2015"><img src="<?php echo get_template_directory_uri(); ?>/images/feature01.jpg" class="img-responsive"></a></span> </div>
+          </div>
+          <div class="col-md-4 col-sm-4">
+            <div class="fea-details"> <span><a href="http://www.cijintl.com/In_The_Press-7052-Freyana.html?zoom_highlight=freyana"><img src="<?php echo get_template_directory_uri(); ?>/images/feature02.jpg" class="img-responsive"></a></span> </div>
+          </div>
+          <div class="col-md-4 col-sm-4">
+            <div class="fea-details"> <span><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/feature03.jpg" class="img-responsive"></a></span> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="footer-block">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-sm-6">
+        <div class="fot-copy">
+          <ul class="list-inline">
+            <li> © 2016 Freyana.com</li>
+            <li><a href="#"> Terms &amp; Conditions</a></li>
+            <li><a href="#"> Privacy Policy</a></li>
+            <li><a href="#"> Returns &amp; Exchanges</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-6 col-sm-6">
+        <div class="fot-copy text-right">
+          <ul class="list-inline">
+            <li><a href="mailto:info@freyana.com"> E : info@freyana.com</a></li>
+            <li><a href="mailto:accounts@freyana.com"> accounts@freyana.com</a></li>
+            <li>P : 1-844-3739262</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="footer-bottom">
+  <div class="container">
+    <div class="row">
+      <div class="top-link">
+        <div class="col-md-2 col-sm-2 bdr"> <img src="<?php echo get_template_directory_uri(); ?>/images/footer-logo.png" class="img-responsive">
+          <address>
+          Freyana, which means "beloved," is the culmination of a mother and daughter’s lifelong passion for Jewellery.
+          </address>
+        </div>
+        <div class="col-md-2 col-sm-2 bdr">
+          <div class="fot-detail">
+            <h3>TOP LINKS</h3>
+            <ul class="list-unstyled">
+              <li><a href="#">ABOUT </a></li>
+              <li><a href="#">BLOG</a></li>
+              <li><a href="#">PRESS</a></li>
+              <li><a href="#">CONTACT</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-2 col-sm-2 bdr">
+          <div class="fot-detail">
+            <h3>My account</h3>
+            <ul class="list-unstyled">
+              <li><a href="#">Login</a></li>
+              <li><a href="#">Register</a></li>
+              <li><a href="#">Cart</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-2 col-sm-2 bdr">
+          <div class="fot-detail bot-so">
+            <h3>Stay With US</h3>
+            <ul class="list-inline">
+              <li><a href="https://www.facebook.com/freyanajewels" class="fa fa-facebook" target="_blank"></a></li>
+              <li><a href="https://twitter.com/freyanajewels" class="fa fa-twitter" target="_blank"></a></li>
+              <li><a href="#" class="fa fa-youtube"></a></li>
+              <li><a href="#" class="fa fa-linkedin"></a></li>
+              <li><a href="#" class="fa fa-flickr"></a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-4">
+          <div class="fot-detail">
+            <h3>the newsletter</h3>
+            <p>Sign up for our newsletter and be the first 
+              to know everything. It’s as simple as that.</p>
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Search for...">
+              <span class="input-group-btn">
+              <button class="btn btn-default" type="button">Go!</button>
+              </span> </div>
+            <!-- /input-group --> 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.min.js"></script> 
+<script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.js"></script> 
+<!-- FlexSlider --> 
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.flexslider-min.js"></script> 
+<script type="text/javascript" charset="utf-8">
+var $ = jQuery.noConflict();
+$(window).load(function() {
+$('.flexslider').flexslider({
+      animation: "fade"
+});
+
+$(function() {
+$('.show_menu').click(function(){
+    $('.menu').fadeIn();
+    $('.show_menu').fadeOut();
+    $('.hide_menu').fadeIn();
+});
+$('.hide_menu').click(function(){
+    $('.menu').fadeOut();
+    $('.show_menu').fadeIn();
+    $('.hide_menu').fadeOut();
+});
+});
+});
+</script><!-- FlexSlider --> 
+<!-- Owl Carousel Thumb auto gallery --> 
+<script src="<?php echo get_template_directory_uri(); ?>/js/owl.carousel.js"></script> 
+<script type="text/javascript">
+$(document).ready(function() {
+  $("#owl-demo").owlCarousel({
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+  });
+  var owl = $("#owl-demo");
+ 
+  owl.owlCarousel();
+ 
+  // Custom Navigation Events
+  $(".next").click(function(){
+    owl.trigger('owl.next');
+  })
+  $(".prev").click(function(){
+    owl.trigger('owl.prev');
+  })
+ 
+});
+</script><!-- Owl Carousel Thumb auto gallery --> 
+<!-- Bootstrap hover:none in responsive --> 
+<script>
+jQuery(function($) {
+    if($(window).width()>769){
+      $('.navbar .dropdown').hover(function() {
+            $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+
+        }, function() {
+            $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+
+        });
+        $('.navbar .dropdown > a').click(function(){
+            location.href = this.href;
+        });
+    }
+});
+</script><!-- Bootstrap hover:none in responsive --> 
+<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.jsmodal.js"></script> 
+<script>
+$( document ).ready(function() {
+   $('#modal-1').modal();
+});
+</script>
+</body>
+</html>
